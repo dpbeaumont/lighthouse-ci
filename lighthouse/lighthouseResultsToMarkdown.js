1842e96dd0f3b7c466dfa85714a968d338f71250
Dave <49551821+dpbeaumont@users.noreply.github.com>
2024-03-12 17:48:25 +0000

    Update lighthouseResultsToMarkdown.js

--- lighthouse/lighthouseResultsToMarkdown.js
+++ lighthouse/lighthouseResultsToMarkdown.js
@@ -8,21 +8,25 @@ fs.readFile('.lighthouseci/assertion-results.json', 'utf8', (error, data) => {
   const results = JSON.parse(data);
 
   let markdown = "## Lighthouse Audit Results\n\n";
-  markdown += "| Audit Item | Expected (%) | Current (%) | Outcome |\n";
+  markdown += "| Audit Item | Target (%) | Current (%) | Outcome |\n";
   markdown += "|------------|--------------|------------|---------|\n";
 
   let actualPercentTotal = 0;
+  let expectedPercentTotal = 0;
 
   results.forEach(item => {
     const expected = (parseFloat(item.expected) * 100).toFixed(0);
     const actual = (item.actual * 100).toFixed(0);
     actualPercentTotal += parseFloat(actual);
+    expectedPercentTotal += parseFloat(expected);
     const outcomeSymbol = item.passed ? '✅' : '❌';
     markdown += `| ${item.auditProperty} | ${expected} | ${actual} | ${outcomeSymbol} |\n`;
   });
 
   const averageActualPercent = (actualPercentTotal / results.length).toFixed(2);
-  markdown += `\n\n#### Average Current Percentage: ${averageActualPercent}%\n`;
+  const averageExpectedPercent = (expectedPercentTotal / results.length).toFixed(2);
+  markdown += `\n\n#### Current Average Percentage: ${averageActualPercent}%\n`;
+  markdown += `#### Target Average Percentage: ${averageExpectedPercent}%\n`;
 
   console.log(markdown);
 });
