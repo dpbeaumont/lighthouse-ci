eb8cdb8acee624c7e47432bae625fefe27ba9e41
Dave <49551821+dpbeaumont@users.noreply.github.com>
2024-04-04 16:08:57 +0100

    Update generateMarkdown.js

--- lighthouse/generateMarkdown.js
+++ lighthouse/generateMarkdown.js
@@ -11,22 +11,13 @@ fs.readFile('.lighthouseci/assertion-results.json', 'utf8', (error, data) => {
   markdown += "| Audit Item | Target (%) | Current (%) | Outcome |\n";
   markdown += "|------------|--------------|------------|---------|\n";
 
-  let actualPercentTotal = 0;
-  let expectedPercentTotal = 0;
-
   results.forEach(item => {
     const expected = (parseFloat(item.expected) * 100).toFixed(0);
     const actual = (item.actual * 100).toFixed(0);
-    actualPercentTotal += parseFloat(actual);
-    expectedPercentTotal += parseFloat(expected);
     const outcomeSymbol = item.passed ? '✅' : '❌';
     markdown += `| ${item.auditProperty} | ${expected} | ${actual} | ${outcomeSymbol} |\n`;
   });
 
-  const averageActualPercent = (actualPercentTotal / results.length).toFixed(2);
-  const averageExpectedPercent = (expectedPercentTotal / results.length).toFixed(2);
-  markdown += `\n\n#### Current Average Percentage: ${averageActualPercent}%\n`;
-  markdown += `#### Target Average Percentage: ${averageExpectedPercent}%`;
-
+  markdown += `\n\n#### Application Average (%): ${averageActualPercent}%\n`;
   console.log(markdown);
 });
